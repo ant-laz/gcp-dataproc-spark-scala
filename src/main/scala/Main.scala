3d437f5d5231a7064165dd95dd89f9bb22aad1c5
ant-laz <alazzaro@google.com>
2024-04-26 10:59:28 +0100

    GCS read + write basic example complete

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 package org.tonyz.com
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.functions.{col, desc}
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -8,11 +9,25 @@ object Main {
       .appName("gcp-spark-scala")
       .getOrCreate()
 
+    //Scala example 1  - Google Cloud Storage read/write
+    //https://cloud.google.com/dataproc/docs/tutorials/gcs-connector-spark-tutorial
+    //https://cloud.google.com/dataproc/docs/concepts/connectors/cloud-storage#use_the_connector
     val df = spark.read
       .option("header",value=true)
       .csv("gs://python-lab-329118-scala-spark/AAPL.csv")
 
-    df.show()
+    val busyDaysDF = df.select("Date", "Volume")
+      .where(col("Volume") > 10000000)
+      .orderBy(desc("Volume"))
+
+    //stop spark creating a SUCCESS file when outputting files to GCS
+    spark.conf.set("mapreduce.fileoutputcommitter.marksuccessfuljobs", "false")
+
+    val writer = busyDaysDF.write
+      .mode("Overwrite")
+      .format("csv")
+
+    writer.save("gs://python-lab-329118-scala-spark/AAPL_filtered.csv")
 
   }
 }
